e8b8bc3df2bf34922778093dc5e1388a8ed447d8
Suman Islam <sumanislam1416@gmail.com>
2022-06-09 16:01:22 +0600

    add solutions of Min Max Sum

---
+++ problem-solving(Basic)/7. Min_Max_Sum.js
@@ -0,0 +1,60 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function miniMaxSum(arr) {
+    // Write your code here
+    const n = arr.length; 
+    let temp = 0;
+    
+    for(let i=0; i<n; i++) {
+        for(let j=0; j<n-i-1; j++) {
+            if(arr[j] > arr[j+1]) {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    
+    let lowestSum = 0;
+    let highestSum = 0;
+    
+    for(let i=0; i<n-1; i++) {
+        lowestSum += arr[i]
+        highestSum += arr[i+1];
+    }
+    
+    console.log(lowestSum, highestSum)
+}
+
+function main() {
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    miniMaxSum(arr);
+}
