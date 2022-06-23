98391833c4566c69709ecdca84f94da05d60f1dc
Suman Islam <sumanislam1416@gmail.com>
2022-06-23 14:11:59 +0600

    add solutions of day6 - javascript dates

---
+++ 10 Days of JavaScript/Day6/javascript-dates.js
@@ -0,0 +1,30 @@
+function getDayName(dateString) {
+	let dayName;
+	// Write your code here
+	const date = new Date(dateString);
+	const day = date.getDay();
+
+	switch (day) {
+		case 0:
+			dayName = "Sunday";
+			break;
+		case 1:
+			dayName = "Monday";
+			break;
+		case 2:
+			dayName = "Tuesday";
+			break;
+		case 3:
+			dayName = "Wednesday";
+			break;
+		case 4:
+			dayName = "Thursday";
+			break;
+		case 5:
+			dayName = "Friday";
+			break;
+		case 6:
+			dayName = "Saturday";
+	}
+	return dayName;
+}
